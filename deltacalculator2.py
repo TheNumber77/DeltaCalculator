1c5f94975f7d979c4bd29389bb2f42ede6630c11
TheNumber77 <45149950+TheNumber77@users.noreply.github.com>
2019-06-07 14:01:15 -0400

    Add files via upload

---
+++ deltacalculator2.py
@@ -0,0 +1,10 @@
+linha = '-' * 42
+titulo = 'DELTA CALCULATOR'
+print(linha)
+print(titulo.center(50))
+print(linha)
+a = int(input('digite o valor de "a": '))
+b = int(input('digite o valor de "b": '))
+c = int(input('digite o valor de "c": '))
+d = b ** 2 - 4 * a * c
+print('o valor de delta é: {}'.format(d))
